8d21e714f9950bc02816f1b20c3a5fed697ef915
Emilis <39080797+bonno42h@users.noreply.github.com>
2019-11-06 02:05:31 +0200

    implement header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import GoogleLogo from '../../assets/google-logo.png';
-import styles from './Header.scss';
+import styles from './Header.module.scss';
 
 const Header = () => {
-  return (<div>
-    <img src={GoogleLogo} className={styles.googleLogo} alt="Google Logo" />
-    <span>Fonts</span>
-  </div>);
-}
-
+  return (
+    <header className={styles.root}>
+      <div className={styles.headerCorner}>
+        <img src={GoogleLogo} className={styles.googleLogo} alt="Google Logo" />
+        <span className={styles.fontsText}> Fonts</span>
+      </div>
+      <nav className={styles.navbar}>
+        <ul>
+          <li><a href="https://fonts.google.com/">catalog</a></li>
+          <li><a href="https://fonts.google.com/featured">featured</a></li>
+          <li><a href="https://design.google/library/google-fonts/?utm_source=Google&utm_medium=Fonts&utm_campaign=Article%20Tab">articles</a></li>
+          <li><a href="https://fonts.google.com/about">about</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import styles from './Landing.module.scss';
 
 const Landing = () => {
   return (
-    <Header />
+    <div className={styles.root}>
+      <Header />
+    </div>
   );
 }
 
